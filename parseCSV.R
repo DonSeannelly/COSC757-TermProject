2fe05306c69c8a0df0a61d2c350264319985835a
Sean Donnelly <sdonne5@students.towson.edu>
2018-04-30 12:20:10 -0400

    Filter BTC data on days SQ is traded

--- parseCSV.R
+++ parseCSV.R
@@ -1,13 +1,15 @@
 # This script parses financial data in .csv format for further analysis.
 # Developed for COSC 757 in the spring of 2018 by Sean Donnelly
 
-input_file <- "data/SQ Jan 2 - Apr 4.csv" 
+sq_input_file <- "data/SQ Jan 2 - Apr 4.csv" 
+btc_input_file <- "data/BTC Jan 1 - Apr 1.csv"
 
 # read input
-input_data <- read.csv(input_file, header=TRUE, sep=",")
-# get number of samples in data
-sample_number <- nrow(input_data)
+sq_input_data <- read.csv(sq_input_file, header=TRUE, sep=",")
+btc_input_data <- read.csv(btc_input_file, header=TRUE, sep=",")
 
+sq_input_data[[1]] <- as.Date(sq_input_data[[1]], format="%m/%d/%Y")
+btc_input_data[[1]] <- as.Date(btc_input_data[[1]], format="%m/%d/%Y")
 
-# input_data[col#]
-print(input_data)
+# A data frame of BTC only on days SQ was traded
+btc_on_trading_days <- subset(btc_input_data, Date %in% sq_input_data$DATE)
